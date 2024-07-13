2300b33f3121b0d87243bc17203539265d00c550
osama <osama.mahmoud.h9@gmail.com>
2024-07-13 18:09:16 +0300

    add readme file

--- keycloak-entrypoint.sh
+++ keycloak-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-/opt/keycloak/bin/kc.sh show-config
 /opt/keycloak/bin/kc.sh build
-exec /opt/keycloak/bin/kc.sh start --https-certificate-file=/etc/x509/https/keycloak.crt --https-certificate-key-file=/etc/x509/https/keycloak.key  --optimized  --verbose
+exec /opt/keycloak/bin/kc.sh start  --optimized  --verbose
